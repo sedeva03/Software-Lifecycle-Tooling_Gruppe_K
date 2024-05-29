b3457e81c8961794c03632edbf32b8b26ed639da
TheSmuggler27 <Kamil.Bienias@outlook.com>
2024-05-29 16:42:05 +0200

    added Constructor TicTacToe

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,2 +1,15 @@
 public class TicTacToe {
+    public Player player1;
+    public Player player2;
+    public Player currentPlayer;
+    public Board board;
+
+    public TicTacToe(Player player1, Player player2, Player currentPlayer, Board board) {
+        this.player1 = new Player('X');
+        this.player2 = new Player('O');
+        this.currentPlayer = player1;
+        this.board = new Board();
+
+    }
+
 }
