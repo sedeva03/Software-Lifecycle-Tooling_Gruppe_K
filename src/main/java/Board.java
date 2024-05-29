9c284c2711e4b15e3d2c29f8ada1d05493c5ec51
nichtkilian <kilianebaumann@gmail.com>
2024-05-29 16:19:46 +0200

    added isCellEmpty, place and isFull methods

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -9,4 +9,27 @@ public class Board {
             }
         }
     }
+
+    public boolean isCellEmpty(int x, int y){
+        if(cells[x][y] == ' '){
+            return true;
+        }
+        return false;
+    }
+
+    public void place(int x, int y, char marker){
+        cells[x][y] = marker;
+    }
+
+    public boolean isFull(){
+        for(int i = 0; i < cells.length; i++){
+            for(int j = 0; j < cells[0].length -1 ; j++){
+                if(cells[i][j] == ' '){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
 }
