230f3d486199d5ab6857f5f276e31ec01327253e
nichtkilian <kilianebaumann@gmail.com>
2024-05-29 16:07:55 +0200

    finished constructor for Board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,2 +1,12 @@
 public class Board {
+    public char [][] cells;
+
+    public Board(){
+        this.cells = new char[3][3];
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                cells[i][j] = ' ';
+            }
+        }
+    }
 }
